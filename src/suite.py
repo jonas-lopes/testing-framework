932afa831e4bce2079af1a8121517f7d1f724cf8
Jonas F. Lopes <jonas.lopes@uemg.br>
2025-05-05 21:15:20 -0300

    5. Classe TestSuite

---
+++ src/suite.py
@@ -0,0 +1,12 @@
+# suite.py
+
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add_test(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
